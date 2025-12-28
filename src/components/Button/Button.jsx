c4106902293699cd6ba5d76fb238d3c4ddb7d374
DeveloperInProgress <thisbevibhor@gmail.com>
2025-12-28 19:06:08 +0530

    Test commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+// Using "children" makes it reusable. 
+// We can pass "Give Feedback" or any other text later.
+const Button = ({ children }) => {
+  return (
+    <button className={styles.button}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
